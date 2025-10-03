46061f5d406f4224a6d541f613f54f9efc54e634
TranQuynh555 <n23dcpt051@student.ptithcm.edu.vn>
2025-10-03 18:32:38 +0700

    Update atm.py

--- Lab08/atm.py
+++ Lab08/atm.py
@@ -1,1 +1,17 @@
+# atm.py
+class ATM:
+    def __init__(self, pin, balance):
+        self.correct_pin = pin
+        self.balance = balance
+
+    def verify_pin(self, input_pin):
+        return input_pin == self.correct_pin
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            return "Invalid amount"
+        if amount > self.balance:
+            return "Insufficient funds"
+        self.balance -= amount
+        return "Withdraw successful"
 
